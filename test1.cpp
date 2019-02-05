70d2f22d095b1a79601b5c2440e90df2e5bc4bb9
Santiago <pinto.santiago.augusto@gmail.com>
2019-02-05 12:53:19 -0300

    added changes in test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -4,5 +4,13 @@ int main(){
 	cout<<"Hello world!"<<endl;
 	cout<<"This is a new line !"<<endl;
 	cout<<"This is a new line from second repo"<<endl;
+
+	for (int i = 0; i<10; ++i){
+
+		if (i%2)
+			cout<<"even"<<endl;
+		else
+			cout<<"odd"<<endl;
+	}
 	return 0;
 }
