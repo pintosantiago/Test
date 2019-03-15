8ea949ca437affe377a3391e6dc4c0f56a0e05eb
Santiago <pinto.santiago.augusto@gmail.com>
2019-03-15 11:23:40 -0300

    Removed unnecessary lines

--- test1.cpp
+++ test1.cpp
@@ -2,24 +2,8 @@
 using namespace std;
 
 
-void foo(){
-	cout<<"This is foo"<<endl;
-}
-
 int main(){
+
 	cout<<"Hello world!"<<endl;
-	cout<<"This is a new line !"<<endl;
-	cout<<"This is a new line from second repo"<<endl;
-
-	for (int i = 0; i<10; ++i){
-
-		if (i == 2)
-			cout<<"two"<<endl;
-		else
-			cout<<"not two"<<endl;
-	}
-
-
-	foo();
 	return 0;
 }
